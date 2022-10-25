211f8d14e4e1a2bf17c42a49a3a6e53ad23b8df0
cas1m1r <tyl3r5durd3n@gmail.com>
2022-10-24 21:30:03 -0400

    roll it up in sqlite
    adding database because this will grow large and disorganized just trying to browse through the JSON alone.

--- processor.py
+++ processor.py
@@ -1,6 +1,10 @@
 import requests 
+import sqlite3
+import pandas
 import json
 import time
+import sys
+import os
 import re
 
 OFF = '\033[0m'
@@ -10,6 +14,8 @@ BLU = '\033[32m'
 YEL = '\033[33m'
 GRN = '\033[34m'
 PRP = '\033[35m'
+
+
 
 
 def create_timestamp():
@@ -58,3 +64,84 @@ def process_page(website):
 		print(f'[+] {website} added cookies:{json.dumps(results["cookies"])}')
 	results['links'] = links
 	return results
+
+
+def load_db():
+	return sqlite3.connect('phishers.db')
+
+def save_db(db):
+	db.commit()
+	db.close()
+
+def create_table():
+	scammers = load_db()
+	# Creating table
+	create_table = """ CREATE TABLE phishing_pages (
+		            IP TEXT NOT NULL,
+		            URL TEXT NOT NULL,
+		            LINK TEXT NOT NULL,
+		            STATUS INT NOT NULL,
+		            COOKIES TEXT NOT NULL
+		        ); """
+	cursor = scammers.cursor()
+	cursor.execute(create_table)
+	cursor.close()
+	save_db(scammers)
+
+def insert_row(ip, data):
+	scammers = load_db()
+	cursor = scammers.cursor()
+	if len(data.keys()) > 1:
+		for link in data.keys():
+			d = data[link]
+			if 'url' not in d.keys():
+				continue
+			try:
+				cookies = f"[{json.dumps(d['cookies'])}]"
+			except KeyError:
+				cookies = '[]'
+				pass
+			try:
+				links = f"[{json.dumps(d['links'])}]"
+			except:
+				links = '[]'
+				pass
+			row = f"('{ip}', '{d['url']}', '{links}', '{d['status']}', '{cookies}')"
+			cmd = f"INSERT INTO phishing_pages VALUES {row}"
+			print(f'Trying to run SQL Command:\n{cmd}')
+			cursor.execute(cmd)		
+	elif len(data.keys()):
+		
+		d = data[list(data.keys()).pop()]
+
+		try:
+			cookies = f"[{json.dumps(d['cookies'])}]"
+		except KeyError:
+			cookies = '[]'
+			pass
+		try:
+			links = f"[{json.dumps(d['links'])}]"
+		except:
+			links = '[]'
+			pass
+		try:
+			row = f"('{ip}', '{d['url']}', '{links}', '{d['status']}', '{cookies}')"
+			cmd = f"INSERT INTO phishing_pages VALUES {row}"
+			print(f'Trying to run SQL Command:\n{cmd}')
+			cursor.execute(cmd)
+		except:
+			pass
+	cursor.close()
+	save_db(scammers)
+
+def main():
+	if not os.path.isfile('phishers.db'):
+		create_table()
+	if len(sys.argv) > 1:
+		data = json.loads(open(sys.argv[1],'r').read())
+		for ip in data.keys():
+			entry = data[ip]
+			insert_row(ip, entry)
+
+if __name__ == '__main__':
+	main()
